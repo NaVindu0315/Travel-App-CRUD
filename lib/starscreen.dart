13d206fbc1a7dc6c288695e68975e5f437b76516
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-05-06 00:32:59 +0530

    widget set

--- lib/starscreen.dart
+++ lib/starscreen.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:travelapp/widget.dart';
 
+import 'displaywidget.dart';
+
 class Ratingclz extends StatefulWidget {
   const Ratingclz({Key? key}) : super(key: key);
 
@@ -18,19 +20,42 @@ class _RatingclzState extends State<Ratingclz> {
       appBar: AppBar(
         title: Text('Star Rating Example'),
       ),
-      body: Center(
-        child: InteractiveRatingWidget(
-          maxRating: 5,
-          filledStar: Icons.star,
-          unfilledStar: Icons.star_border,
-          onChanged: (rating) async {
-            print('Rating changed to $rating');
-            final failedattempt = _firestore.collection("stars").doc('start1');
-            failedattempt.set({
-              'starcount': rating,
-            });
-          },
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          Center(
+            child: Row(
+              children: [
+                Spacer(),
+                StarDisplay(
+                  key: ValueKey('key'),
+                  value: 3,
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Center(
+            child: InteractiveRatingWidget(
+              maxRating: 5,
+              filledStar: Icons.star,
+              unfilledStar: Icons.star_border,
+              onChanged: (rating) async {
+                print('Rating changed to $rating');
+                final failedattempt =
+                    _firestore.collection("stars").doc('start1');
+                failedattempt.set({
+                  'starcount': rating,
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
