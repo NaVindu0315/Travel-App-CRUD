2e0f40ed161de75ed9367587af2da9b22456eb90
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-05-07 21:25:55 +0530

    gemini working

--- lib/chatai.dart
+++ lib/chatai.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
+import 'package:travelapp/consts.dart';
 
 class promptpage extends StatefulWidget {
   const promptpage({Key? key}) : super(key: key);
@@ -11,6 +14,35 @@ class promptpage extends StatefulWidget {
 }
 
 class _promptpageState extends State<promptpage> {
+  String msg = "";
+  Future<void> genprmpot() async {
+    try {
+      final model = await _createGenerativeModel();
+      final content = [Content.text(promptmsg)];
+      final response = await model.generateContent(content);
+      if (response != null) {
+        msg = response.text!;
+
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.success,
+          title: 'Success',
+          text: '$msg',
+          autoCloseDuration: const Duration(seconds: 8),
+        );
+        print(msg);
+      } else {
+        print('Error: Response is null');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<GenerativeModel> _createGenerativeModel() async {
+    return GenerativeModel(model: 'gemini-pro', apiKey: apikey);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +55,8 @@ class _promptpageState extends State<promptpage> {
                 Spacer(),
                 ElevatedButton(
                     onPressed: () {
-                      print('pal');
+                      //print('pal');
+                      genprmpot();
                     },
                     child: Text('Prompt')),
                 Spacer(),
